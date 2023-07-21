e591d9c4df9866dfb3400e70d5585c5ccea6c12c
Alien663 <rite2844@gmail.com>
2023-07-21 10:44:20 +0800

    feat:Remove Test Page

--- Code/src/App.jsx
+++ Code/src/App.jsx
@@ -6,7 +6,6 @@ import Sample from './components/Sample'
 import Parent from './components/Parent';
 import Counter from './components/Counter';
 import FakeCallAPI from './components/FakeCallAPI';
-import QAQ from './components/Test';
 import Main from './components/Main'
 
 
@@ -18,7 +17,6 @@ function App() {
       <Route path='/Parent' element={<Parent />}></Route>
       <Route path='/Counter' element={<Counter />}></Route>
       <Route path='/apisample' element={<FakeCallAPI />}></Route>
-      <Route path='/test' element={<QAQ />}></Route>
       <Route path='/' element={ <Main /> }></Route>
     </Routes>
   )
